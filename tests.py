234aa2886f5fe2a32a9ccbc7c028375d06994594
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-03-15 15:02:26 +0530

    Confirming to PEP-8 standerds. Added travis-ci
    Please use pylint before further development.
    Adding support for automated testing.

---
+++ tests.py
@@ -0,0 +1,15 @@
+"""
+    Test cases to test travis-ci integration.
+"""
+
+import unittest
+
+import courses_portal_iiit.new_cour
+
+
+class TestCoursesModule(unittest.TestCase):
+    """ Test Module """
+
+    def test_none(self):
+        """ Tests if module can be imported is created """
+        pass
